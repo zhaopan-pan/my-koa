0c064cff922a0b62855ed6d35515822267145ab6
Administrator <zhaopanhw@foxmail.com>
2019-10-22 18:10:51 +0800

    添加上传中间件

--- koa.js
+++ koa.js
@@ -1,5 +1,16 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+const koaStatic = require('koa-static');
+const koaBody = require('koa-body');//文件保存库
+const Router = require('koa-router');
 const Koa = require('koa');
 let app = new Koa();
+var router = new Router();
+
+
+const port = process.env.PORT || '3000';
+const uploadHost = `http://localhost:${port}/uploads/`;
 
 const middleware1 = async (ctx, next) => {
     console.log(1);
@@ -7,25 +18,56 @@ const middleware1 = async (ctx, next) => {
     console.log(6);
 }
 
-const middleware2 = async (ctx, next) => {
-    console.log(2);
-    await next();
-    console.log(5);
-}
+app.use(koaBody({
+    formidable: {
+        //设置文件的默认保存目录，不设置则保存在系统临时目录下  os
+        uploadDir: path.resolve(__dirname, '../../uploads')
+    },
+    multipart: true // 开启文件上传，默认是关闭
+}));
+//开启静态文件访问
+app.use(koaStatic(
+    path.resolve(__dirname, './static')
+));
+//文件二次处理，修改名称
+router.post('/upload', (ctx, next) => {
+    var file = ctx.request.files ? ctx.request.files.f1 : null;//得道文件对象
+    if (!file || file.size === 0) {
+        return ctx.body = { 'msg': "无文件" };
+    }
+    var path = file.path;
+    var fname = file.name;//原文件名称
+    var nextPath = path + fname;
+    //得到扩展名
+    var extArr = fname.split('.');
+    var ext = extArr[extArr.length - 1];
+    // var nextPath = path + '111.' + ext;
+    var nextPath = path.slice(-(path.lastIndexOf('upload')-1));
+    //重命名文件
+    fs.renameSync(path, nextPath);
+    //以 json 形式输出上传文件地址 
+    ctx.body = `{
+      "fileUrl":"${uploadHost}${nextPath.slice(nextPath.lastIndexOf('/') + 1)}"
+    }`;
 
-const middleware3 = async (ctx, next) => {
-    console.log(3);
-    await next();
-    console.log(4);
-}
+});
+let aa = "upload_4586cdbecb1cefb56a1f52d610143b7e.docx";
+console.log(aa.slice(aa.lastIndexOf('.') + 1));
+// router.get("/index",(ctx,next)=>{
+// })
+app
+    .use(router.routes())
+    .use(router.allowedMethods());
 
-app.use(middleware1);
-app.use(middleware2);
-app.use(middleware3);
-app.use(async (ctx, next) => {
-    ctx.body = 'hello world'
-})
+app.on('error', (err, ctx) => {
+    console.error('server error', err, ctx);
+});
+//  app.listen(port)
+/**
+* http server
+*/
+var server = http.createServer(app.callback());
+server.listen(port);
 
-app.listen(3001)
 
 
--- promise-koa.js
+++ promise-koa.js
@@ -1,75 +1,38 @@
-
-function compose(middleware) {
-
-    if (!Array.isArray(middleware)) {
-        throw new TypeError('Middleware stack must be an array!');
-    }
-
-    return function (ctx, next) {
-        let index = -1;
-
-        return dispatch(0);
-
-        function dispatch(i) {
-            if (i < index) {
-                return Promise.reject(new Error('next() called multiple times'));
-            }
-            index = i;
-
-            let fn = middleware[i];
-
-            if (i === middleware.length) {
-                fn = next;
-            }
-
-            if (!fn) {
-                return Promise.resolve();
-            }
-
-            try {
-                return Promise.resolve(fn(ctx, () => {
-                    return dispatch(i + 1);
-                }));
-            } catch (err) {
-                return Promise.reject(err);
-            }
-        }
-    };
-}
-
-
-let middleware = [];
 let context = {
     data: []
 };
 
-middleware.push(async (ctx, next) => {
+async function middleware1(ctx, next) {
     console.log('action 001');
     ctx.data.push(1);
     await next();
     console.log('action 006');
-    ctx.data.push(11);
-});
+    ctx.data.push(6);
+}
 
-middleware.push(async (ctx, next) => {
+async function middleware2(ctx, next) {
     console.log('action 002');
     ctx.data.push(2);
     await next();
     console.log('action 005');
-    ctx.data.push(22);
-});
+    ctx.data.push(5);
+}
 
-middleware.push(async (ctx, next) => {
+async function middleware3(ctx, next) {
     console.log('action 003');
     ctx.data.push(3);
     await next();
     console.log('action 004');
-    ctx.data.push(33);
-});
+    ctx.data.push(4);
+}
 
-const fn = compose(middleware);
-
-fn(context)
+Promise.resolve(middleware1(context, () => {
+    return Promise.resolve(middleware2(context, () => {
+        return Promise.resolve(middleware3(context, () => {
+            return Promise.resolve();
+        }));
+    }));
+}))
     .then(() => {
         console.log('end');
         console.log('context = ', context);
